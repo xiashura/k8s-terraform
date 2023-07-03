cbe5cdadd6a79c489edb524bc80d9a99a0b27917
xiashura <plexanov@protonmail.com>
2023-07-03 11:15:48 +0300

    add charts for istio

--- main.tf
+++ main.tf
@@ -14,11 +14,53 @@ resource "helm_release" "cni" {
   values = [
     templatefile("${path.root}/charts/cilium.yml", var.cilium)
   ]
-
-  timeouts {
-    create = "1h30m"
-    update = "2h"
-    delete = "20m"
-  }
+  depends_on = [kind_cluster.workshop]
 }
+
+resource "helm_release" "istio" {
+
+  name             = "istio"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "base"
+  create_namespace = true
+  wait             = true
+  wait_for_jobs    = true
+  namespace        = "istio-system"
+  # values = [
+  #   templatefile("", "")
+  # ]
+  depends_on = [helm_release.cni]
+}
+
+
+resource "helm_release" "istiod" {
+
+  name             = "istiod"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "istiod"
+  create_namespace = true
+  wait             = true
+  wait_for_jobs    = true
+  namespace        = "istio-system"
+  # values = [
+  #   templatefile("", "")
+  # ]
+  depends_on = [helm_release.istio]
+}
+
+resource "helm_release" "istio_ingress" {
+
+  name             = "istio-ingress"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "gateway"
+  create_namespace = true
+  wait             = true
+  wait_for_jobs    = true
+  namespace        = "istio-ingress"
+  # values = [
+  #   templatefile("", "")
+  # ]
+  depends_on = [helm_release.istio]
+}
+
 
